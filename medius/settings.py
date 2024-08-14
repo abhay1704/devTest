ee83f6abc11b6ebddd5a0ae4623f640889f72f68
Abhay Pratap Singh <abhaypratapninth@gmail.com>
2024-08-14 11:07:44 +0530

    fe

--- medius/settings.py
+++ medius/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/xlAnalyser/"
+STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "xlAnalyser/static/xlAnalyser")
+    os.path.join(BASE_DIR, "xlAnalyser/static/")
 ]
 
 # Default primary key field type
